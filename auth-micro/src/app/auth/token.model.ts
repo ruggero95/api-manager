1290f05811922e6737f513a2ff7913b31c336fa4
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-02 11:20:43 +0100

    token and user mongoose model

---
+++ auth-micro/src/app/auth/token.model.ts
@@ -0,0 +1,28 @@
+import { CostantEnum } from "./../../config/constants";
+import mongoose, { Types } from "mongoose"
+
+interface IToken{
+    token:string;
+    userId:Types.ObjectId;
+    createdAt: any,
+}
+const tokenSchema = new mongoose.Schema<IToken>({
+    token:{
+        type:String,
+        required:true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+        expires: CostantEnum.TOKEN_EXPIRY
+    }
+}, {timestamps: true});
+
+
+export const Token = mongoose.model<IToken>('Tokens',tokenSchema,'Tokens')
