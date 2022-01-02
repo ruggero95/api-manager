46f7b9fb94caf5b74c813cee724e9f1e0904d7ae
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-02 12:28:12 +0100

    added checluser and logout

--- auth-micro/src/app/auth/auth.service.ts
+++ auth-micro/src/app/auth/auth.service.ts
@@ -1,32 +1,48 @@
 import { logger } from 'core/logger'
-import { User } from './../user/user.model'
+import { IUser, User } from './../user/user.model'
 import { Token } from "./token.model"
+import jwt from "jsonwebtoken"
+import { AuthFailureError } from 'core/errorResponse'
+
 export class AuthService {
     public async login(username: string, password: string) {
-        let user = await User.findOne({ username: username })
+        let user:IUser = await User.findOne({ username: username })
         const result = user ?  await user.comparePassword(password) : false
         if (!result) {
             //create userif not exist
             logger.info('creo utente')
             user = await this.createUser(username, password)
         }
-
         const token = user.genereteAccessToken()
-        console.log(token)
-        const tokenModel = await new Token({token: token, userId: user.id})
+        const tokenModel = await new Token({token: token, userId: user._id})
         await tokenModel.save()
         return token
     }
 
-    public async createUser(username: string, password: string) {
+    public async createUser(username: string, password: string): Promise<IUser> {
         const user = new User({ username: username, password: password,tokens:[] })
-        console.log(user)
-        await user.save().catch((err)=>{console.log('errore'); console.log(err)})
-        console.log('saved')
+        await user.save()
         return user
     }
 
-    public async checkUser(token: string) {
+    public async checkUser(tokenString: string):Promise<boolean> {
         //check user con il token
+        try{
+            const payload:any = await jwt.verify(tokenString, process.env.ACCESS_TOKEN)
+            const token = await Token.findOne({token:tokenString, userId:payload.id ? payload.id : ''})
+            if(!token || (Array.isArray(token) && token.length==0)){
+                throw(new AuthFailureError('Token not registered'))
+            }            
+            return true
+        }catch(e){
+            throw(new AuthFailureError('Token not valid'))
+        }
+      
+
+    }
+
+    public async logout(tokenString:string): Promise<boolean>{
+        const token = await Token.deleteMany({token:tokenString})
+        return true
     }
 }
