1d2085051456c94a1906462c6f1dbaaf4c227fe9
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-03 12:01:20 +0100

    styles and vuetify

---
+++ gui-api-manager/src/router/index.js
@@ -0,0 +1,111 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import { authService } from '@/app/auth/auth.service'
+import {store} from "@/app/mystore"
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'dashboard',
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    meta:{
+      requiresAuth:true
+    },
+    component: () => import('@/views/dashboard/Dashboard.vue'),
+  },
+  {
+    path: '/typography',
+    name: 'typography',
+    component: () => import('@/views/typography/Typography.vue'),
+  },
+  {
+    path: '/icons',
+    name: 'icons',
+    component: () => import('@/views/icons/Icons.vue'),
+  },
+  {
+    path: '/cards',
+    name: 'cards',
+    component: () => import('@/views/cards/Card.vue'),
+  },
+  {
+    path: '/simple-table',
+    name: 'simple-table',
+    component: () => import('@/views/simple-table/SimpleTable.vue'),
+  },
+  {
+    path: '/form-layouts',
+    name: 'form-layouts',
+    component: () => import('@/views/form-layouts/FormLayouts.vue'),
+  },
+  {
+    path: '/pages/account-settings',
+    name: 'pages-account-settings',
+    component: () => import('@/views/pages/account-settings/AccountSettings.vue'),
+  },
+  {
+    path: '/login',
+    name: 'pages-login',
+    component: () => import('@/views/pages/Login.vue'),
+    meta: {
+      layout: 'blank',
+    },
+  },
+  {
+    path: '/pages/register',
+    name: 'pages-register',
+    component: () => import('@/views/pages/Register.vue'),
+    meta: {
+      layout: 'blank',
+    },
+  },
+  {
+    path: '/error-404',
+    name: 'error-404',
+    component: () => import('@/views/Error.vue'),
+    meta: {
+      layout: 'blank',
+    },
+  },
+  {
+    path: '*',
+    redirect: 'error-404',
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+})
+
+router.beforeEach(async (to, from, next)=>{    
+  if(to.meta.requiresAuth){
+    const login = await authService.checkLoginVue()
+    if(!login){
+      return next(next({
+        path: '/login',
+        params: { nextUrl: to.fullPath }
+      }))
+    }        
+  }
+  
+  if(to.name=='pages-login'){
+    const login = await authService.checkLoginVue()
+    if(login){
+      return next(next({
+        path: '/dashboard',
+        params: { nextUrl: to.fullPath }
+      }))
+    } 
+    
+  }
+  return next()
+})
+
+export default router
+
