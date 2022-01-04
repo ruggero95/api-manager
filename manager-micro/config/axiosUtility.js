7d2cfe030409a3f3d6f7687cb4b5f4fb903bee3a
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-04 01:05:04 +0100

    axios and response utility

---
+++ manager-micro/config/axiosUtility.js
@@ -0,0 +1,30 @@
+const axiosUtility = {
+    getTokenHeader: (token) => {
+        return { Authorization: `Bearer ${token}` }
+    },
+    getFullHeader: (token) => {
+        return {
+            headers: {
+                'Content-Type': 'application/json',
+                ...utils.getTokenHeader(token)
+            }
+        }
+    },
+    handleAxiosError: (error) => {
+        console.log('axios error')
+        console.log(error)
+    },
+    getResponsePayload: (response) => {
+        return response.data ? response.data : null
+    },
+    hanldeAxiosResponse: (response, customHandler) => {
+        const payload = utils.getResponsePayload(response)
+        if (!payload) {
+            console.log('axios error, missing payload')
+            return
+        }
+        return customHandler(payload)
+    }
+}
+
+module.exports = axiosUtility
