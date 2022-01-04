8aaefdf800c454f464656e0c9241407afc679cca
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-04 21:45:06 +0100

    importing all controllers

--- manager-micro/app/index.controller.js
+++ manager-micro/app/index.controller.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const planRouter = require('./plan/plan.controller')
-
-router.use('/plans',planRouter)
+const requestRouter = require('./requests/request.controller')
+const authMiddleware = require('./auth/auth.middleware')
+router.use('/plans', authMiddleware.checkAuthToken, planRouter)
+router.use(requestRouter)
 
 module.exports = router
