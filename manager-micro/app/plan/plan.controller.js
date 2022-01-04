5030ef23914331bdf66e756399c1e8e58fefd547
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-04 01:06:18 +0100

    plan logic

---
+++ manager-micro/app/plan/plan.controller.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const planRepository = require('./plan.repository')
+const planService = require('./plan.service')
+const {successResponse, errorResponse, badRequestResponse} = require('./../../config/response')
+router.post('/add', async (req, res, next)=>{
+    try{
+        const user_id = req.body.user_id
+        const name = req.body.name
+        if(!user_id || !name){
+            return badRequestResponse(res, 'Missing params')
+        }
+        const plan = await planService.create(user_id,name)
+        if(plan){
+            return successResponse(res,'Plan created')
+        }
+        return errorResponse(res, 'Error creating plan')
+    }catch(e){
+        next(e)
+    }
+})
+
+
+
+router.get('/key/:api_key', async (req, res, next)=>{
+    try{
+        const api_key = req.params.api_key
+        if(!api_key){
+            return badRequestResponse(res,'Missing apy key')
+        }
+        const { rows } = await planRepository.getByApiKey(api_key)
+        if(rows && rows[0]){
+            return successResponse(res, 'Plans', rows)
+        }
+        return errorResponse(res, 'Missing plans',null)
+    }catch(e){
+        next(e)
+    }
+})
+
+router.get('/user/:user_id', async (req, res, next)=>{
+    try{
+        const user_id = req.params.user_id
+        if(!user_id){
+            return badRequestResponse(res,'Missing user id')
+        }
+        const { rows } = await planRepository.getByUserID(user_id)
+        if(rows && rows[0]){
+            return successResponse(res, 'Plans', rows)
+        }
+        return errorResponse(res, 'Missing plans',null)
+    }catch(e){
+        next(e)
+    }
+})
+
+module.exports = router
