0e507d5b7fbd61ce0471faefd6cbcd71f0df46ba
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-05 15:12:57 +0100

    fix error on invalid token

--- manager-micro/app/auth/auth.middleware.js
+++ manager-micro/app/auth/auth.middleware.js
@@ -4,19 +4,23 @@ const authApi = require('./auth.api')
 const authMiddleware = {
     checkAuthToken: async (req, res, next) => {
         // Get the token data
-        const authHeader = req.headers['authorization']
-        const token = authHeader && authHeader.split(' ')[1]
-        if(!token){
-            return unauthorizedResponse(res)
+        try{
+            const authHeader = req.headers['authorization']
+            const token = authHeader && authHeader.split(' ')[1]
+            if(!token){
+                return unauthorizedResponse(res)
+            }
+            const checkToken = await authApi.checkToken(token)
+            
+            req.body.user_id = checkToken.data.payload.id
+            if(!checkToken || checkToken.error == 'true'){
+                return unauthorizedResponse(res)
+            }
+            next()
+        }catch(e){
+            unauthorizedResponse(res)
         }
-        const checkToken = await authApi.checkToken(token)
-        console.log(checkToken)
-        console.log(checkToken.data.payload.id)
-        req.body.user_id = checkToken.data.payload.id
-        if(!checkToken || checkToken.error == 'true'){
-            return unauthorizedResponse(res)
-        }
-        next()
+        
     }
 }
 
