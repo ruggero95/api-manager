4a8fac8939a1f98d412989a9ae88f2dfa7a45290
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-07 00:08:44 +0100

    managing try api in store

--- gui-api-manager/src/app/mystore.js
+++ gui-api-manager/src/app/mystore.js
@@ -1,8 +1,19 @@
 import { reactive } from "@vue/composition-api"
+//user token payload
+//plans all plans
+//requests all rqeuests of all plan, the query already contains all plan details (unecessary)
+/*preview saving try of the api like:
+response replaced at each try of that plan
+    {
+        plan_id:
+        response:[]
+    }
+*/
 export const store = {
     state:reactive({
         user:null,
         plans:[],
-        requests:[]
+        requests:[],
+        preview:[]
     })
 }
