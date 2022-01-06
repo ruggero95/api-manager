a53ad0112c870ca907f96560272f602a3ee70e90
DESKTOP-9D0LQ9C\rugge <ruggero.panzeri95@gmail.com>
2022-01-07 00:09:12 +0100

    try api logic

--- gui-api-manager/src/app/manager/manager.api.js
+++ gui-api-manager/src/app/manager/manager.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { env } from "../config/const"
 import { utils } from "../config/utils"
 export const managerApi = {
-    getPlansByUserID:(user_id)=>{        
+    getPlansByUserID:()=>{        
         return axios.get(`${env.api_manager_backend}/plans/user`,utils.getFullHeader(localStorage.getItem('token'))).then((response)=>{
             return utils.hanldeAxiosResponse(response, (payload)=>{
                return payload.data ? payload.data : []
